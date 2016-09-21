30a64f56a68b84c7ff59c6305398ea7702b82b19
lingen.liu <lingen.liu@gmail.com>
2016-09-21 20:56:46 +0800

    完善细节

--- PandaHttpSwiftTests/PandaHttpSwiftTests.swift
+++ PandaHttpSwiftTests/PandaHttpSwiftTests.swift
@@ -13,18 +13,16 @@ class PandaHttpSwiftTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testGetJson() {
+    func testGetJson() throws {
         let request:OPHRequest = OPHRequest.jsonRequest(url: "http://openpanda.org:8081/account/search?search=l&page=1&pagesize=10", method: .OPH_HTTP_GET)
         
-        let response:OPHResponse = try! OPHNetWork.sharedInstance().syncRequest(request: request)
+        let response:OPHResponse = try OPHNetWork.sharedInstance().syncRequest(request: request)
         
         if response.isRequestOk() {
             let result:String? = response.exceptedStringResult()
@@ -32,7 +30,7 @@ class PandaHttpSwiftTests: XCTestCase {
         }
     }
     
-    func testPostJson() {
+    func testPostJson() throws {
         let url:String = "http://openpanda.org:8081/account";
         
         let params:Dictionary<String,Any> = [
@@ -45,7 +43,7 @@ class PandaHttpSwiftTests: XCTestCase {
         
         let request:OPHRequest = OPHRequest.jsonRequest(url: url, method: .OPH_HTTP_POST, params: params)
         
-        let response:OPHResponse = try! OPHNetWork.sharedInstance().syncRequest(request: request)
+        let response:OPHResponse = try OPHNetWork.sharedInstance().syncRequest(request: request)
         
         if response.isRequestOk() {
             let result = response.exceptedStringResult()
@@ -55,7 +53,7 @@ class PandaHttpSwiftTests: XCTestCase {
     }
     
     
-    func testPutJson() {
+    func testPutJson() throws {
         let url:String = "http://openpanda.org:8081//account/changePwd";
         
         let params:Dictionary<String,Any> = [
@@ -66,7 +64,7 @@ class PandaHttpSwiftTests: XCTestCase {
         
         let request:OPHRequest = OPHRequest.jsonRequest(url: url, method: .OPH_HTTP_PUT, params: params)
         
-        let response:OPHResponse = try! OPHNetWork.sharedInstance().syncRequest(request: request)
+        let response:OPHResponse = try OPHNetWork.sharedInstance().syncRequest(request: request)
         
         if response.isRequestOk() {
             let result = response.excepedDictionayResult()
